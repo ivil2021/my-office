2978679663bbc687055a118a04bfb483408c72b3
ivil2021 <89508514326@mail.ru>
2025-07-31 14:40:16 +0300

    Create Modals Folder And Move There User Create And Edit Code

--- src/components/UserTable/index.tsx
+++ src/components/UserTable/index.tsx
@@ -12,12 +12,13 @@
 
 import { useState, useMemo } from 'react';
 import useSWR, { mutate } from 'swr';
-import { Table, Button, Space, Modal } from 'antd';
+import { Table, Button, Space } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { getAllUsers, createUser, deleteUser, editOneUser } from '../../api/user';
 import { useRegistrationForm } from "../../hooks/useRegistrationForm";
-import { UserTableContainer, FormContainer, TextError } from './index.styles';
+import { UserTableContainer } from './index.styles';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { CreateUser } from '../../modals/user/CreateUser';
 
 export function UserTable () {
   const { form, setFormValues } = useRegistrationForm();
@@ -160,91 +161,12 @@ export function UserTable () {
       >
         Создать пользователя
       </Button>
-      <Modal
-        title="Создание нового пользователя"
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <FormContainer>
-          <div>
-            <label htmlFor="name">Имя: </label>
-            <input
-              name="name"
-              value={form.values.name}
-              onChange={form.handleChange}
-              onBlur={form.handleBlur}
-            />
-            {form.errors.name && form.touched.name && (
-              <TextError>
-                <div>{form.errors.name}</div>
-              </TextError>
-            )}
-          </div>
-
-          <div>
-            <label htmlFor="lastName">Фамилия: </label>
-            <input
-              name="lastName"
-              value={form.values.lastName}
-              onChange={form.handleChange}
-              onBlur={form.handleBlur}
-            />
-            {form.errors.lastName && form.touched.lastName && (
-              <TextError>
-                <div>{form.errors.lastName}</div>
-              </TextError>
-            )}
-          </div>
-
-          <div>
-            <label htmlFor="age">Возраст: </label>
-            <input
-              name="age"
-              value={form.values.age}
-              onChange={form.handleChange}
-              onBlur={form.handleBlur}
-            />
-            {form.errors.age && form.touched.age && (
-              <TextError>
-                <div>{form.errors.age}</div>
-              </TextError>
-            )}
-          </div>
-
-          <div>
-            <label htmlFor="phone">Телефон: </label>
-            <input
-              name="phone"
-              value={form.values.phone}
-              onChange={form.handleChange}
-              onBlur={form.handleBlur}
-            />
-            {form.errors.phone && form.touched.phone && (
-              <TextError>
-                <div>{form.errors.phone}</div>
-              </TextError>
-            )}
-          </div>
-
-          <div>
-            <label htmlFor="email">Email: </label>
-            <input
-              name="email"
-              value={form.values.email}
-              onChange={form.handleChange}
-              onBlur={form.handleBlur}
-              disabled={!form.values.name || Boolean(form.errors.name)}
-            />
-            {form.errors.email && form.touched.email && (
-              <TextError>
-                <div>{form.errors.email}</div>
-              </TextError>
-            )}
-          </div>
-
-        </FormContainer>
-      </Modal>
+      <CreateUser
+        isModalOpen={isModalOpen}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+        form={form}
+      />
       <Table 
         columns={columns}
         dataSource={users}
---
+++ src/modals/user/CreateUser.tsx
@@ -0,0 +1,92 @@
+import { Modal } from 'antd';
+import { FormContainer, TextError } from './index.styles';
+
+export function CreateUser({ isModalOpen, handleOk, handleCancel, form }: any) {
+  return (
+    <Modal
+        title="Создание нового пользователя"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <FormContainer>
+          <div>
+            <label htmlFor="name">Имя: </label>
+            <input
+              name="name"
+              value={form.values.name}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            {form.errors.name && form.touched.name && (
+              <TextError>
+                <div>{form.errors.name}</div>
+              </TextError>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Фамилия: </label>
+            <input
+              name="lastName"
+              value={form.values.lastName}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            {form.errors.lastName && form.touched.lastName && (
+              <TextError>
+                <div>{form.errors.lastName}</div>
+              </TextError>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="age">Возраст: </label>
+            <input
+              name="age"
+              value={form.values.age}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            {form.errors.age && form.touched.age && (
+              <TextError>
+                <div>{form.errors.age}</div>
+              </TextError>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="phone">Телефон: </label>
+            <input
+              name="phone"
+              value={form.values.phone}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            {form.errors.phone && form.touched.phone && (
+              <TextError>
+                <div>{form.errors.phone}</div>
+              </TextError>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              name="email"
+              value={form.values.email}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+              disabled={!form.values.name || Boolean(form.errors.name)}
+            />
+            {form.errors.email && form.touched.email && (
+              <TextError>
+                <div>{form.errors.email}</div>
+              </TextError>
+            )}
+          </div>
+
+        </FormContainer>
+      </Modal>
+  );
+}
