70a9be91b9bd6209052d8a1f1c45d6050e8eb23d
ivil2021 <89508514326@mail.ru>
2025-09-01 21:25:52 +0300

    Fix Modal Validation

--- src/modals/user/useCreateAndEditUser.ts
+++ src/modals/user/useCreateAndEditUser.ts
@@ -4,25 +4,32 @@ import { mutate } from 'swr';
 import { createUser, editUser } from '../../api/user';
 import type { IUser } from "../../entities/user";
 
+const noSpecialSymbolsRegex = /^[a-zA-Z0-9]+$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 const validationSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'Имя должно быть не меньше 3 символов!')
     .max(20, 'Имя слишком длинное!')
+    .matches(noSpecialSymbolsRegex, 'Имя не должно содержать специальных символов')
     .required('Имя обязательно'),
   lastName: Yup.string()
     .min(3, 'Фамилия должна быть не меньше 3 символов!')
     .max(20, 'Фамилия слишком длинная!')
+    .matches(noSpecialSymbolsRegex, 'Фамилия не должна содержать специальных символов')
     .required('Фамилия обязательна'),
   age: Yup.string()
     .min(1, 'Возраст должен быть не меньше 1 символов!')
     .max(3, 'Возраст слишком длинный!')
+    .matches(noSpecialSymbolsRegex, 'Возраст не должен содержать специальных символов')
     .required('Возраст обязателен'),
   phone: Yup.string()
     .min(11, 'Номер телефона должен быть не меньше 11 символов!')
     .max(11, 'Номер телефона должен быть не больше 11 символов!')
+    .matches(noSpecialSymbolsRegex, 'Номер телефона не должен содержать специальных символов')
     .required('Номер телефона обязателен'),
   email: Yup.string()
-    .email('Неверный формат email')
+    .matches(emailRegex, 'Некорректный формат email')
     .required('Email обязателен')
 });
 
